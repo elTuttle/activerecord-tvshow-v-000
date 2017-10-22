964213387147e72bbeb46191d269b2d4d49a8ac8
John Tuttle <thejwt@gmail.com>
2017-10-22 06:31:34 +0000

    automatically backed up by learn

---
+++ db/migrate/002_add_season_to_shows.rb
@@ -0,0 +1,12 @@
+class AddSeasonToShows < ActiveRecord::Migration
+
+  def change
+    create_table :shows do |t|
+      t.string :name
+      t.string :network
+      t.string :day
+      t.integer :rating
+    end
+  end
+
+end
